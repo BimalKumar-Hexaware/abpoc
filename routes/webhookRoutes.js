d365b3c59e2a25b932aac47843e213b8f8edae1c
Bimal Kumar <40321@hexaware.com>
2018-07-03 12:01:21 +0530

    added welcome intent response

--- routes/webhookRoutes.js
+++ routes/webhookRoutes.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res, next) {
-  
-  res.send('respond with a resource');
+  console.log('Dialogflow Request body: ' + JSON.stringify(req.body));
+  switch (req.body.inputs.intent) {
+    case "actions.intent.MAIN":
+      res.json({
+        "textToSpeech": "Hi welcome to micro strategy. I am Emily, your virtual assistant. Please choose how can I help you",
+        "ssml": "Hi welcome to micro strategy. I am Emily, your virtual assistant. Please choose how can I help you",
+        "displayText": "Hi welcome to micro strategy. I am Emily, your virtual assistant. Please choose how can I help you"
+      }).end();
+      break;
+  }
 });
 
 module.exports = router;
