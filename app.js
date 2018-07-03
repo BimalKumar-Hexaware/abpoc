dc27efccacb2359303e7cdc7bc0bc03406427478
Bimal Kumar <40321@hexaware.com>
2018-07-03 16:13:52 +0530

    actions on google added

--- app.js
+++ app.js
@@ -1,41 +1,20 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var webhookRoutes = require('./routes/webhookRoutes');
+const express = require('express')
+const bodyParser = require('body-parser')
+const { dialogflow } = require('actions-on-google')
+const port = process.env.PORT || 3000;
 
 var app = express();
+const dialogflowApp = dialogflow();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/webhook', webhookRoutes);
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
+dialogflowApp.intent('Default Welcome Intent', conv => {
+  conv.ask('Hi, how is it going?')
+  conv.ask(`Here's a picture of a cat`)
+  conv.ask(new Image({
+    url: 'https://developers.google.com/web/fundamentals/accessibility/semantics-builtin/imgs/160204193356-01-cat-500.jpg',
+    alt: 'A cat',
+  }))
 });
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+app.use(bodyParser.json(), dialogflowApp).listen(port, function () {
+  console.log('App listening on port:' + port);
+})
