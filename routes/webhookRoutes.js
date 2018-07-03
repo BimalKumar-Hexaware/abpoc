a6afb51a2317db50caca3b96bea1790a1de45985
Bimal Kumar <40321@hexaware.com>
2018-07-03 12:26:40 +0530

    response changed

--- routes/webhookRoutes.js
+++ routes/webhookRoutes.js
@@ -5,10 +5,9 @@ router.post('/', function (req, res, next) {
   console.log('Dialogflow Request body: ' + JSON.stringify(req.body));
   switch (req.body.queryResult.action) {
     case "input.welcome":
+      console.log("*************Inside welcome intent*************");
       res.json({
-        "textToSpeech": "Hi welcome to micro strategy. I am Emily, your virtual assistant. Please choose how can I help you",
-        "ssml": "Hi welcome to micro strategy. I am Emily, your virtual assistant. Please choose how can I help you",
-        "displayText": "Hi welcome to micro strategy. I am Emily, your virtual assistant. Please choose how can I help you"
+        "fulfillmentText": "Hi welcome to micro strategy. I am Emily, your virtual assistant. Please choose how can I help you"
       }).end();
       break;
   }
