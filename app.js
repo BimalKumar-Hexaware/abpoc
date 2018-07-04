d3f737197d4023476cc28033225a332de6db6da6
Bimal Kumar <40321@hexaware.com>
2018-07-04 11:44:29 +0530

    added list payload

--- app.js
+++ app.js
@@ -9,6 +9,25 @@ const dialogflowApp = dialogflow();
 dialogflowApp.intent('Default Welcome Intent', conv => {
   conv.ask('Hi welcome to micro strategy. I am Emily, your virtual assistant. Please tell me how can I help you');
   //conv.followup('mainmenu_event');
+  conv.ask(new List({
+    title: 'Please choose',
+    items: {
+      [SELECTION_KEY_GET_CALENDAR]: {
+        synonyms: [
+          'Get calendar details',
+        ],
+        title: 'Get calendar details',
+        description: 'Lets you retrieve calendar events',
+      },
+      [SELECTION_KEY_MODIFY_CALENDAR]: {
+        synonyms: [
+          'Modify calendar events',
+        ],
+        title: 'Modify calendar events',
+        description: 'Lets you modify calendar events'
+      },
+    },
+  }));
 });
 
 app.use(bodyParser.json(), dialogflowApp).listen(port, function () {
