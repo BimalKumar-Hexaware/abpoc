1f200762ccd0f8c87602134037d0b9e4877c88d2
Bimal Kumar <40321@hexaware.com>
2018-07-05 15:58:20 +0530

    addeed fallback handler code

--- dialogflowHandlers.js
+++ dialogflowHandlers.js
@@ -1,18 +1,18 @@
 const { dialogflow, List } = require('actions-on-google')
-const app = dialogflow({ debug: true });
+const app = dialogflow();
 app.intent('Default Welcome Intent', conv => {
     conv.ask('Hi welcome to micro strategy. I am Emily, your virtual assistant. Please tell me how can I help you');
     conv.ask(new List({
         title: 'Please choose',
         items: {
-            ['get calendar events']: {
+            ['SELECTION_KEY_GET_CALENDAR_EVENTS']: {
                 synonyms: [
                     'Get calendar events',
                 ],
                 title: 'Get calendar events',
                 description: 'Lets you retrieve calendar events',
             },
-            ['modify calendar events']: {
+            ['SELECTION_KEY_MODIFY_EVENTS']: {
                 synonyms: [
                     'Modify calendar events',
                 ],
@@ -23,15 +23,15 @@ app.intent('Default Welcome Intent', conv => {
     }));
 });
 
-app.intent('Default Fallback Intent',(conv)=>{
-    console.log("*****************************************Fallback fired*****************************************");
+app.intent('Default Fallback Intent', (conv) => {
+    console.log(conv);
 });
 
 app.intent('ab.getCalanderEventsQuery', (conv, params) => {
-    conv.ask('Are you looking for a particular meeting? please provide the date and time');
+    conv.ask('I have your calendar details. Please provide the date and time');
 });
 
-app.intent('ab.getCalanderEventsQuery-getDateAndTime', (conv) => {
+app.intent('ab.getCalanderDateAndTime', (conv) => {
     console.log(conv.body.queryResult.action);
     conv.ask('You have a meeting with Alliance Bernstein at 9:30 over webex, a meeting with MR.John Doe at 11:00 in his office in New York office room 342');
 });
