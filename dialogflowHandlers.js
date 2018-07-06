f5d37179b3ac30e20035b8fd40dc8d6e6c542ccc
Bimal Kumar <40321@hexaware.com>
2018-07-06 11:39:00 +0530

    added mofify event intent

--- dialogflowHandlers.js
+++ dialogflowHandlers.js
@@ -30,25 +30,31 @@ app.intent('Default Fallback Intent', (conv) => {
             conv.followup('getcalendardetails-event');
             break;
         case 'SELECTION_KEY_MODIFY_EVENTS':
-
+            conv.followup('modifycalendardetails-event');
             break;
     }
 });
 
-app.intent('ab.getCalanderEventsQuery', (conv, params) => {
-    conv.ask('I have your calendar details. Please provide the date and time');
+app.intent('ab.getCalanderEventSelected', (conv, params) => {
+    conv.ask('Sure. Please provide the date and time');
+});
+
+app.intent('ab.modifyCalendarEventSelected', (conv, params) => {
+    conv.ask('I can do it for you. Please provide the date, scheduled time and the new time of the meeting');
 });
 
 app.intent('ab.getCalanderEventsQuery-getInfo', (conv) => {
     var parameters = conv.parameters;
     console.log("parameters", parameters);
-    var date_period = parameters.date_period;
     var date_time = parameters.date_time;
-    var date = parameters.date;
-    var time = parameters.time;
-    var time_period = parameters.time_period;
-    console.log("date_period", date_period);
     conv.ask('You have a meeting with Alliance Bernstein at 9:30 over webex, a meeting with MR.John Doe at 11:00 in his office in New York office room 342');
+});
+
+app.intent('ab.modifyCalendarEventSelected-getInfo', (conv) => {
+    var parameters = conv.parameters;
+    console.log("parameters", parameters);
+    var messages = ["You have an conflict with your calendar  timing at 2:00", "Your meeting has been moved to the requested time"];
+    conv.ask(messages[Math.floor(Math.random() * messages.length)]);
 });
 
 module.exports = app;
