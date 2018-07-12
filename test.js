440a31a775ce2bb677606eef26355a6ba0ec65f0
Bimal Kumar <40321@hexaware.com>
2018-07-12 10:47:42 +0530

    test webhook added

--- test.js
+++ test.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
+const port = 8880;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/webhook/api', (req, res) => res.send('Hello World!'))
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(port, () => console.log('Example app listening on port 3000!'))
