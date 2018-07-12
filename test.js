eca74cc9451e6b727cc9b9457a56bae8c511cd6c
Bimal Kumar <40321@hexaware.com>
2018-07-12 10:51:29 +0530

    test webhook added

--- test.js
+++ test.js
@@ -3,6 +3,9 @@ const app = express()
 const port = 8880;
 
 app.get('/', (req, res) => res.send('Hello World!'));
-app.get('/webhook/api', (req, res) => res.send('Hello World!'))
+app.get('/webhook/api', (req, res) => {
+    console.log('Dialogflow Request body: ' + JSON.stringify(req.body));
+    res.send('Hello World!')
+});
 
 app.listen(port, () => console.log('Example app listening on port 3000!'))
