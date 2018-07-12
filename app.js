4aa0b5d8157692a404d1a1bba88292d48114d9dd
Bimal Kumar <40321@hexaware.com>
2018-07-12 14:11:08 +0530

    added code changes

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const port = 8880;
-
+var express = require('express');
+var bodyParser = require('body-parser');
+var routes = require('./routes');
 var app = express();
-const dialogflowApp = require('./dialogflowHandlers');
+var port = process.env.PORT || 8880;
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+app.use(routes);
+var server = app.listen(port, function () {
+	console.log("Application started listening port " + port);
+});
 
-app.use(bodyParser.json(), dialogflowApp).listen(port, function () {
-  console.log('App listening on port:' + port);
-})
+
