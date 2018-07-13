01d8de2c81c95e727903b696ded576dd7a54b27e
Bimal Kumar <40321@hexaware.com>
2018-07-13 14:03:42 +0530

    bug fix

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var session = require('express-session');
-app.use(session({ secret: 'this-is-a-secret-token',resave: true, saveUninitialized: true, cookie: { maxAge: 60000 }}));
+
 var routes = require('./routes');
 var app = express();
 var port = process.env.PORT || 80;
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
+app.use(session({ secret: 'this-is-a-secret-token',resave: true, saveUninitialized: true, cookie: { maxAge: 60000 }}));
 app.use(routes);
 app.listen(port, function () {
 	console.log("Application started listening port " + port);
