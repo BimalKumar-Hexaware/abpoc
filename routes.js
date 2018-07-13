8538137c1ebd0237e2955592d6cdef6c2f4af700
Bimal Kumar <40321@hexaware.com>
2018-07-13 11:40:27 +0530

    bug fix

--- routes.js
+++ routes.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var config = require('./config.js');
 const { WebhookClient, Text, Card, Payload, Suggestion } = require('dialogflow-fulfillment');
+const { dialogflow, List } = require('actions-on-google')
+const app = dialogflow({ debug: true });
 
 router.post('/api/webhook', function (req, res) {
 	console.log("request", JSON.stringify(req.body));
@@ -16,14 +18,32 @@ router.post('/api/webhook', function (req, res) {
 			agent.add(new Suggestion('Work Day'));
 			agent.add(new Text('Hi welcome to micro strategy. I am Emily, your virtual assistant. How can I help you'));
 		});
-	}*/
-	const agent = new WebhookClient({ request: req, response: res });
+	}
+	const agent = new WebhookClient({ request: req, response: res });*/
 	var intent = req.body.inputs.intent;
 	console.log("intent", intent);
-	if(intent == "actions.intent.MAIN") {
-		agent.add(new Text('Hi welcome to micro strategy. I am Emily, your virtual assistant. How can I help you'));
+	if (intent == "actions.intent.MAIN") {
+		conv.ask('Hi welcome to micro strategy. I am Emily, your virtual assistant. Please tell me how can I help you');
+		conv.ask(new List({
+			title: 'Please choose',
+			items: {
+				['SELECTION_KEY_GET_CALENDAR_EVENTS']: {
+					synonyms: [
+						'Get calendar events',
+					],
+					title: 'Get calendar events',
+					description: 'Lets you retrieve calendar events',
+				},
+				['SELECTION_KEY_MODIFY_EVENTS']: {
+					synonyms: [
+						'Modify calendar events',
+					],
+					title: 'Modify calendar events',
+					description: 'Lets you modify calendar events'
+				},
+			},
+		}));
 	}
-	agent.handleRequest(intentMap);
 });
 
 module.exports = router;
