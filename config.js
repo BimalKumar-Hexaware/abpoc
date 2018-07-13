878a2a5de77649d280dfdb34cfb56d5aaa2a881e
Bimal Kumar <40321@hexaware.com>
2018-07-13 14:39:13 +0530

    bug fix

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	'accessToken': "fb28796ec675402a99b7a97dd04002a1",
-	'dialogFlowAPI': "https://dialogflow.googleapis.com/v2/sessions:detectIntent",
+	'dialogFlowAPI': "https://dialogflow.googleapis.com/v2/123456789:detectIntent",
 	'intents': ['Default Welcome Intent']
 }
 
--- routes.js
+++ routes.js
@@ -33,7 +33,7 @@ router.post('/api/webhook', function (req, res) {
 		null
 	);
 	jwtClient.authorize((err, tokens) => {
-		request.post(config.dialogFlowAPI.replace('sessions', '123456789'), {
+		request.post(config.dialogFlowAPI, {
 			'auth': {
 				'bearer': tokens.access_token,
 			},
