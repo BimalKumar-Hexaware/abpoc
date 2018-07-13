5257d9d8370cbe50bd93ae1157e126fe1610d0e4
Bimal Kumar <40321@hexaware.com>
2018-07-13 14:47:24 +0530

    h

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	'accessToken': "fb28796ec675402a99b7a97dd04002a1",
-	'dialogFlowAPI': "https://dialogflow.googleapis.com/v2/projects/abmspoc-e6fa7/agent/sessions/ddbe0c1a-e0b2-b74b-c35b-22a512ad3956:detectIntent",
+	'dialogFlowAPI': "https://dialogflow.googleapis.com/v2/projects/abmspoc-e6fa7/agent/ddbe0c1a-e0b2-b74b-c35b-22a512ad3956:detectIntent",
 	'intents': ['Default Welcome Intent']
 }
 
