1a9d4937b5a3fabecca2c8807478c0d8f2ef4346
Bimal Kumar <40321@hexaware.com>
2018-07-17 14:07:19 +0530

    vv

--- dialogflowHandlers.js
+++ dialogflowHandlers.js
@@ -25,51 +25,6 @@ app.intent('actions.intent.MAIN', conv => {
     }));
 });
 
-app.intent('Default Welcome Intent', conv => {
-    conv.ask('Hi welcome to micro strategy. I am Emily, your virtual assistant. Please tell me how can I help you');
-    conv.ask(new List({
-        title: 'Please choose',
-        items: {
-            ['SELECTION_KEY_GET_CALENDAR_EVENTS']: {
-                synonyms: [
-                    'Get calendar events',
-                ],
-                title: 'Get calendar events',
-                description: 'Lets you retrieve calendar events',
-            },
-            ['SELECTION_KEY_MODIFY_EVENTS']: {
-                synonyms: [
-                    'Modify calendar events',
-                ],
-                title: 'Modify calendar events',
-                description: 'Lets you modify calendar events'
-            },
-        },
-    }));
-});
-
-app.intent('Default Fallback Intent', (conv) => {
-    console.log("Selected option key", conv.arguments.raw.input.OPTION.textValue);
-    switch (conv.arguments.raw.input.OPTION.textValue) {
-        case 'SELECTION_KEY_GET_CALENDAR_EVENTS':
-            return helper.queryDialogflow("get calendar events").then((result) => {
-                console.log('dfrersult', JSON.stringify(result));
-                conv.ask(result.fulfillment.messages[0].textToSpeech);
-            }).catch((err) => {
-                res.send(err);
-            })
-            break;
-        case 'SELECTION_KEY_MODIFY_EVENTS':
-            return helper.queryDialogflow("get calendar events").then((result) => {
-                console.log('dfrersult', JSON.stringify(result));
-                conv.ask(result.fulfillment.messages[0].textToSpeech);
-            }).catch((err) => {
-                res.send(err);
-            })
-            break;
-    }
-});
-
 app.intent('actions.intent.OPTION', (conv, params, option) => {
     console.log(option);
     switch (option) {
@@ -88,8 +43,8 @@ app.intent('actions.intent.OPTION', (conv, params, option) => {
             }).catch((err) => {
                 res.send(err);
             })
-    }
-    conv.ask('You did not select any item');
+            break;
+    }    
 });
 
 
