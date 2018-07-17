8f50b3419efa9fbe7e6e01e83af96f8a356ed4b8
Bimal Kumar <40321@hexaware.com>
2018-07-17 11:37:45 +0530

    mainn tnet t added

--- dialogflowHandlers.js
+++ dialogflowHandlers.js
@@ -1,5 +1,28 @@
 const { dialogflow, List, actionssdk } = require('actions-on-google')
 const app = actionssdk();
+app.intent('actions.intent.MAIN', conv => {
+    conv.ask('Hi welcome to micro strategy. I am Emily, your virtual assistant. Please tell me how can I help you');
+    conv.ask(new List({
+        title: 'Please choose',
+        items: {
+            ['SELECTION_KEY_GET_CALENDAR_EVENTS']: {
+                synonyms: [
+                    'Get calendar events',
+                ],
+                title: 'Get calendar events',
+                description: 'Lets you retrieve calendar events',
+            },
+            ['SELECTION_KEY_MODIFY_EVENTS']: {
+                synonyms: [
+                    'Modify calendar events',
+                ],
+                title: 'Modify calendar events',
+                description: 'Lets you modify calendar events'
+            },
+        },
+    }));
+});
+
 app.intent('Default Welcome Intent', conv => {
     conv.ask('Hi welcome to micro strategy. I am Emily, your virtual assistant. Please tell me how can I help you');
     conv.ask(new List({
