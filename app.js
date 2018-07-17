bde6feb28630ab3cbb50efbc8b7e36494843829f
Bimal Kumar <40321@hexaware.com>
2018-07-17 11:32:27 +0530

    actions adk added

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
-var routes = require('./routes');
+var actionsdkApp = require('./dialogflowHandlers');
+//var routes = require('./routes');
 var app = express();
 var port = process.env.PORT || 8880;
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
-app.use(routes);
+app.use(actionsdkApp);
+//app.use(routes);
 app.listen(port, function () {
 	console.log("Application started listening port " + port);
 });
